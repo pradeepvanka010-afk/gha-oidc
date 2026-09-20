b06d21fb18ecd47f2335416bc53d0d1c81d035c3
pradeepvanka010-afk <pradeepvanka010@gmail.com>
2026-09-20 18:09:25 +0530

    m1

--- prod/main.tf
+++ prod/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "gha-oidc-bucket"
+    bucket = "oidc-aws-gha-bucket"
     key = "prod/terraform.tfstate"
     region = "ap-south-1"
     
